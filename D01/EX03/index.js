faea503e0c7495ea02eea07c3171c23a8bbe47f7
Jonathan Cohen <96384721+JoniCohen@users.noreply.github.com>
2022-06-19 22:09:37 +0300

    Update index.js

--- D01/EX03/index.js
+++ D01/EX03/index.js
@@ -1,17 +1,16 @@
 function compareNumbers(Number1, Number2){
     if(Number1 > Number2 ){
-        console.log('The Number 1 is bigger than the Number 2');
-    }
+        console.log(Number1+ ' is bigger than ' +Number2);}
+        
     else if(Number1 === Number2){
-        console.log('Number 1 and Number 2 are the same number');
+        console.log(Number1+ ' and '+ Number2+ ' are the same number');
     }
     else{
-        console.log('The Number 1 is smaller than the Number 2');
+        console.log(Number1+ ' is smaller than '+Number2);
     }
 
 }
-    compareNumbers(2,3);
-}
+    compareNumbers(2,1);
 
 // Do not remove or change this line, or the tests won't work
 export { compareNumbers };
