968b56ca3fe5c216abb7e8332a85d7b0e2b0d5f5
Jonathan Cohen <96384721+JoniCohen@users.noreply.github.com>
2022-06-19 18:02:34 +0300

    Update index.js

--- D01/EX03/index.js
+++ D01/EX03/index.js
@@ -1,5 +1,16 @@
-function compareNumbers() {
-  //your code
+function compareNumbers(Number1, Number2){
+    if(Number1 > Number2 ){
+        console.log('The Number 1 is bigger than the Number 2');
+    }
+    else if(Number1 === Number2){
+        console.log('Number 1 and Number 2 are the same number');
+    }
+    else{
+        console.log('The Number 1 is smaller than the Number 2');
+    }
+
+}
+    compareNumbers(2,3);
 }
 
 // Do not remove or change this line, or the tests won't work
