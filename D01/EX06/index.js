4328fb049bb6c81000982dfcf39013a02a7180eb
Jonathan Cohen <96384721+JoniCohen@users.noreply.github.com>
2022-06-19 22:15:46 +0300

    Update index.js

--- D01/EX06/index.js
+++ D01/EX06/index.js
@@ -1,20 +1,20 @@
 const output = document.getElementById('output');
 function compareNumbers(Number1, Number2){
     if(Number1 > Number2 ){
-        document.write('The Number 1 is bigger than the Number 2');
+        document.write(Number1+ ' is bigger than '+Number2);
     }
     else if(Number1 === Number2){
-        document.write('Number 1 and Number 2 are the same number');
+        document.write(Number1+ ' and '+Number2+ ' are the same number');
     }
     else{
-        document.write('The Number 1 is smaller than the Number 2');
+        document.write(Number1+ ' is smaller than '+Number2);
     }
 
 }
 
 function modulo(Number1, Number2){
     if(Number1 > Number2 && (Number1%Number2 !== 0)){
-    document.write('The modulo of Number 1 % Number 2 is: '+ Number1%Number2);
+    document.write('The modulo of '+Number1+ ' % '+Number2 + ' is: '+ Number1%Number2);
     }
     else if(Number1 > Number2 && Number1%Number2 ===0){
         compareNumbers(Number1,Number2);
