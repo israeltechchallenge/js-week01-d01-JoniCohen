ebe00cc8e35f55c46dea37cfa01b03f78c85612c
Jonathan Cohen <96384721+JoniCohen@users.noreply.github.com>
2022-06-19 17:56:04 +0300

    Update index.js

--- D01/EX02/index.js
+++ D01/EX02/index.js
@@ -1,5 +1,18 @@
 function isBiggerThan() {
-  //your code
+  const Number = 4;
+function isBiggerThan(NewNumber){
+    if(NewNumber > Number ){
+        console.log('The new number is bigger than 4')
+    }
+    else if(NewNumber === Number){
+        console.log('The new number is equal to 4')
+    }
+    else{
+        console.log('The new number is smaller than 4 ')
+    }
+
+    }
+    isBiggerThan(3);
 }
 
 // Do not remove or change this line, or the tests won't work
