5b15ccfc5e740cafbc21113ccdc9060685aac5ae
Jonathan Cohen <96384721+JoniCohen@users.noreply.github.com>
2022-06-19 22:12:54 +0300

    Update index.js

--- D01/EX04/index.js
+++ D01/EX04/index.js
@@ -1,19 +1,19 @@
 function compareNumbers(Number1, Number2){
     if(Number1 > Number2 ){
-        console.log('The Number 1 is bigger than the Number 2');
+        console.log(Number1+ ' is bigger than '+Number2);
     }
     else if(Number1 === Number2){
-        console.log('Number 1 and Number 2 are the same number');
+        console.log(Number1+ ' and '+ Number2+ ' are the same number');
     }
     else{
-        console.log('The Number 1 is smaller than the Number 2');
+        console.log(Number1+ ' is smaller than '+Number2);
     }
 
 }
 
 function modulo(Number1, Number2){
     if(Number1 > Number2 && (Number1%Number2 !== 0)){
-    console.log('The modulo of Number 1 % Number 2 is: '+ Number1%Number2);
+    console.log('The modulo of '+ Number1+ ' % '+ Number2+ ' is: '+ Number1%Number2);
     }
     else if(Number1 > Number2 && Number1%Number2 ===0){
         compareNumbers(Number1,Number2);
@@ -25,7 +25,7 @@ function modulo(Number1, Number2){
         compareNumbers(Number1,Number2);
     }
     }
-    modulo(4,5);
-modulo(4,5);
+    modulo(4,3);
+
 // Do not remove or change this line, or the tests won't work
 export { modulo };
